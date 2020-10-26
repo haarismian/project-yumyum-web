e4416d19f6da6a6c3ea32413c0ba777dcc6bb47c
Haaris Mian <mail@haarismian.com>
2020-10-25 22:57:00 -0400

    db done

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,7 +1,11 @@
 CREATE TABLE recipes (
-  id BIGSERIAL NOT NULL,
+  id BIGSERIAL NOT NULL PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   cuisine VARCHAR(50) NOT NULL,
-  price_range INT NOT NULL check (price_range >= 1 and price_range <=5)
+  price_range INT NOT NULL check (
+    price_range >= 1
+    and price_range <= 5
+  )
 );
-
+INSERT INTO recipes (name, cuisine, price_range)
+VALUES ('Butter Chicken', 'Indian', 2);
